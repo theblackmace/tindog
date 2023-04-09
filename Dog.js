a6b84cb100b2b5847532085769cb802990b9c040
theblackmace <52634803+theblackmace@users.noreply.github.com>
2023-04-09 23:18:17 +0530

    7

--- Dog.js
+++ Dog.js
@@ -6,14 +6,7 @@ class Dog {
     }
     getProfileHtml() {
         return `
-            <img src="" class="badge hide" id="badge">
-            <style>
-            .profile-pic {
-                background-image: url(${this.avatar});
-            }
-            </style>
-            <h2 id="name-age">${this.name}, ${this.age}</h2>
-            <p id="bio">${this.bio}</p>
+            <div class="profile-pic" id="profile-pic" style="background-image: url('${this.avatar}');">
                 <img src="" class="badge hide" id="badge">
                 <h2 id="name-age">${this.name}, ${this.age}</h2>
                 <p id="bio">${this.bio}</p>
