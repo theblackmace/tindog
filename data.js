fda0d00ab3060d573d0a8831c182b064f14adcca
theblackmace <52634803+theblackmace@users.noreply.github.com>
2023-04-09 21:35:11 +0530

    4

--- data.js
+++ data.js
@@ -1,21 +1,21 @@
 const dogs = [
     {
         name: "Rex",
-        avatar: "images/dog-rex.jpg",
+        avatar: "./images/dog-rex.jpg",
         age: 25,
         bio: "Art. Literature. Natural wine. Yoga.",
         hasBeenSwiped: false,
         hasBeenLiked: false
     },{
         name: "Bella",
-        avatar: "images/dog-bella.jpg",
+        avatar: "./images/dog-bella.jpg",
         age: 43,
         bio: "Yup, that's my owner. U can meet him if you want",
         hasBeenSwiped: false,
         hasBeenLiked: false
     },{
         name: "Teddy",
-        avatar: "images/dog-teddy.jpg",
+        avatar: "./images/dog-teddy.jpg",
         age: 30,
         bio: "How you doin?",
         hasBeenSwiped: false,
